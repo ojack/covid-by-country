e1c6aca2e3c9c16f9dfb8df9013c0f38cb80b031
Olivia <ojack@Olivias-MacBook-Pro.local>
2020-11-06 12:54:38 +0100

    ease for tooltip

--- app/tooltip.js
+++ app/tooltip.js
@@ -5,9 +5,9 @@ module.exports = (state, emit) => {
     const t = state.tooltip.content
     const point = state.tooltip.point
     const yOff = point[1] > 300 ? point[1] - 200 : point[1] + 100
-    const xOff = point[0] - 80
+    const xOff = point[0] > window.innerWidth - 300 ? window.innerWidth - 300: point[0]
     const {x, y} = state.plot
-    return html`<div class="absolute pa2 f6 w5" style="top:${yOff}px;left:${point[0]}px;background:rgba(100, 100, 100, 0.9)">
+    return html`<div class="absolute pa2 f6 w5" style="top:${yOff}px;left:${xOff}px;pointer-events:none;background:rgba(100, 100, 100, 0.9);transition:top 0.4s, left 0.4s">
       <div class="flex justify-between"><span>${t.name} </span><span class="f7">${state.data.dates[state.dateIndex]}</span></div>
       <div>${Math.round(y.value(t, state.dateIndex)*10)/10} ${y.label}</div>
       <div>${Math.round(x.value(t, state.dateIndex)*10)/10} ${x.label}</div>
